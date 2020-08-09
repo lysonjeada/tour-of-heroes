c72b4075eab057488ad1d2c5242f52f1e6fdc96b
lysbaldrez <lyscalefi@gmail.com>
2020-08-09 17:36:01 -0400

    alo testando

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+
+
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
